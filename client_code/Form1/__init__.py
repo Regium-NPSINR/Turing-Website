843c77220297af7c36e687b74da0528573fff94d
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-13 08:30:07 +0000

    buttton callback

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,3 +11,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def on_score_button_click(self, **event_args):
+      """This method is called when the button is clicked"""
+      pass
+
