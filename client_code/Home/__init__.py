39f857721db79cb1384a25a7a3ab7119418b6634
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-13 08:39:34 +0000

    Created Home; Edited settings; Deleted Form1

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomeTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form1(Form1Template):
+class Home(HomeTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
