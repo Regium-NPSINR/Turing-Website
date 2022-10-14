16f6bc5d88cfd9e22c344d6d618dcde0856e984b
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-14 06:33:56 +0000

    Edited Form1.ItemTemplate1 and Form1; Deleted Form1.RowTemplate1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,13 +6,24 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 class Form1(Form1Template):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
+    
+        # Any code you write here will run when the form opens.
+    
+    def timer_1_tick(self, **event_args):
+        """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+        time -= 1
+        self.time_remaining.text = str(time)
+    
+    def timer_button_click(self, **event_args):
+        """This method is called when the button is clicked"""
+        if self.timer_button.text == "Start Timer":
+            self.timer_1.interval = 1
+            self.timer_button.text == "Pause Timer"
+        elif self.timer_button.text == "Pause Timer":
+            self.
+          
 
-    # Any code you write here will run when the form opens.
-
-  def timer_1_tick(self, **event_args):
-      """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-      pass
 
