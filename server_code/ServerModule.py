84e14a81a7653dff0c28e20a27831962083b326c
NPS INR Cyber Association <112054161+NPSINRCyberAssociation@users.noreply.github.com>
2022-10-14 11:32:52 +0530

    Add server functions

--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -16,14 +16,50 @@ import anvil.server
 #   return 42
 #
 
+@anvil.server.callable
+def add_team(team_no, team_name):
+	if app_tables.leaderboard.search(team_no = team_no):
+		return False
+	
+	if app_tables.leaderboard.search(team_name = team_name):
+		return False
+
+	app_tables.leaderboard.add_row(
+		team_no = team_no, 
+		team_name = 'Test Team',
+		team_score = 0
+	)
+	return True
+
 
 @anvil.server.callable
-def set_play_score(team_no, score):
+def set_score(team_no, score):
 	# set team's score to new score
-	print(team_no, score)
-	app_tables.leaderboard.add_row(
-        team_no = team_no, 
-        team_name = 'Test Team',
-        team_score = score
-    )
-	return app_tables.leaderboard.search()
+	if not app_tables.leaderboard.search(team_no = team_no):
+		return False
+	
+	# Team already exists
+	app_tables.leaderboard.get(team_no = team_no)[score] = score
+	return True
+
+@anvil.server.callable
+def solve_easy(team_no):
+	# set team's score to new score
+	if not app_tables.leaderboard.search(team_no = team_no):
+		return False
+
+	# Team already exists
+	app_tables.leaderboard.get(team_no = team_no)[score] += 1
+	return True
+
+@anvil.server.callable
+def solve_hard(team_no_1, team_no_2):
+	# check if the teams exist
+	if ((not app_tables.leaderboard.search(team_no_1 = team_no_1)) or
+		not app_tables.leaderboard.search(team_no_2 = team_no_2)):
+		return False
+
+	# Team already exists
+	app_tables.leaderboard.get(team_no_1 = team_no_1)[score] += 1
+	app_tables.leaderboard.get(team_no_2 = team_no_2)[score] += 1
+	return True
