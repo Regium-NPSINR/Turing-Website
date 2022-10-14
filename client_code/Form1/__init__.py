3ba0081e681d49ebb82e9d8bcff4042ada0a214e
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-14 06:02:33 +0000

    Edited Form1.ItemTemplate1 and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,3 +11,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def timer_1_tick(self, **event_args):
+      """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+      pass
+
