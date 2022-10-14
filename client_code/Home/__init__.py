de13dbf5eb5cce96a5eddf66b8f7c50cc39175c5
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-14 04:00:30 +0000

    Edited ServerModule and Home

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -16,5 +16,5 @@ class Home(HomeTemplate):
 		"""This method is called when the button is clicked"""
 		team_no = 0
 		score = int(self.score_text_box.text)
-		for i in anvil.server.call('set_play_score', team_no, 'Test Team', score):
+		for i in anvil.server.call('set_play_score', team_no, score):
 			alert(f'Team no.: {i[0]}\nTeam name: {i[1]}\nScore: {i[2]}')
