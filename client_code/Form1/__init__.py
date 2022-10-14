eadc8bf063f59b3404c679b28554ecb9e52e9692
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-14 06:40:19 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,17 +4,20 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from datetime import timedelta
 
 class Form1(Form1Template):
+    
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
     
         # Any code you write here will run when the form opens.
+        self.time_left = 648000
     
     def timer_1_tick(self, **event_args):
         """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-        time -= 1
+        self.time_left -= 1
         self.time_remaining.text = str(time)
     
     def timer_button_click(self, **event_args):
