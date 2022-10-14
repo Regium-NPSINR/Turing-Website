d1cf3a38df2214dd9ac1365aeb4730d3b6472194
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-14 06:42:10 +0000

    rename home form to servertest

--- client_code/ServerTest/__init__.py
+++ client_code/ServerTest/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import HomeTemplate
+from ._anvil_designer import ServerTestTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Home(HomeTemplate):
+class ServerTest(ServerTestTemplate):
 	def __init__(self, **properties):
 		# Set Form properties and Data Bindings.
 		self.init_components(**properties)
