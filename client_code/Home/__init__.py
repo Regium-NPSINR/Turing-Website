2111e700ca7192a62a3fb5f8ff19e98526695cae
NPS INR Cyber Association <112054161+NPSINRCyberAssociation@users.noreply.github.com>
2022-10-14 12:03:23 +0530

    add server changes

--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -11,11 +11,25 @@ class Home(HomeTemplate):
 		self.init_components(**properties)
 
 		# Any code you write here will run when the form opens.
+	def add_team(self, **event_args):
+		"""This method is called when the button is clicked"""
+		team_no = int(self.team_1.text)
+		team_name = self.team_name.text
+		print(anvil.server.call('add_team', team_no, team_name))
 
 	def on_score_button_click(self, **event_args):
 		"""This method is called when the button is clicked"""
-		team_no = 0
+		team_no = int(self.team_1.text)
 		score = int(self.score_text_box.text)
-		for i in anvil.server.call('set_play_score', team_no, score):
-			alert(dict(i))
+		print(anvil.server.call('set_score', team_no, score))
 
+	def on_easy_click(self, **event_args):
+		"""This method is called when the button is clicked"""
+		team_no = int(self.team_1.text)
+		print(anvil.server.call('solve_easy', team_no))
+
+	def on_hard_click(self, **event_args):
+		"""This method is called when the button is clicked"""
+		team_no_1 = int(self.team_1.text)
+		team_no_2 = int(self.team_2.text)
+		print(anvil.server.call('solve_hard', team_no_1, team_no_2))
--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -18,48 +18,60 @@ import anvil.server
 
 @anvil.server.callable
 def add_team(team_no, team_name):
-	if app_tables.leaderboard.search(team_no = team_no):
+	return 'Called'
+	# check if the team exists
+	if app_tables.leaderboard.get(team_no = team_no) != None:
 		return False
 	
-	if app_tables.leaderboard.search(team_name = team_name):
+	if app_tables.leaderboard.get(team_name = team_name) != None:
 		return False
 
 	app_tables.leaderboard.add_row(
 		team_no = team_no, 
-		team_name = 'Test Team',
+		team_name = team_name,
 		team_score = 0
 	)
 	return True
 
-
 @anvil.server.callable
 def set_score(team_no, score):
-	# set team's score to new score
-	if not app_tables.leaderboard.search(team_no = team_no):
+	# check if the team exists
+	if app_tables.leaderboard.get(team_no = team_no) == None:
 		return False
 	
 	# Team already exists
-	app_tables.leaderboard.get(team_no = team_no)[score] = score
+	app_tables.leaderboard.get(team_no = team_no)[team_score] = score
 	return True
 
 @anvil.server.callable
 def solve_easy(team_no):
-	# set team's score to new score
-	if not app_tables.leaderboard.search(team_no = team_no):
+	# check if the team exists
+	if app_tables.leaderboard.get(team_no = team_no) == None:
 		return False
 
 	# Team already exists
-	app_tables.leaderboard.get(team_no = team_no)[score] += 1
+	print(app_tables.leaderboard.get(team_no = team_no))
+	print(dict(app_tables.leaderboard.get(team_no = team_no)))
+	app_tables.leaderboard.get(team_no = team_no)[team_score] += 1
 	return True
 
 @anvil.server.callable
 def solve_hard(team_no_1, team_no_2):
 	# check if the teams exist
-	if ((not app_tables.leaderboard.search(team_no_1 = team_no_1)) or
-		not app_tables.leaderboard.search(team_no_2 = team_no_2)):
+	if ((app_tables.leaderboard.get(team_no = team_no_1) == None) or
+		(app_tables.leaderboard.get(team_no = team_no_2) == None)):
 		return False
 
-	# Team already exists
-	app_tables.leaderboard.get(team_no_1 = team_no_1)[score] += 1
-	app_tables.leaderboard.get(team_no_2 = team_no_2)[score] += 1
+	# Teams already exists
+	app_tables.leaderboard.get(team_no = team_no_1)[team_score] += 1
+	app_tables.leaderboard.get(team_no = team_no_2)[team_score] -= 1
 	return True
+
+@anvil.server.callable
+def get_score(team_no):
+	# check if the team exists
+	if app_tables.leaderboard.get(team_no = team_no) == None:
+		return False
+
+	# Team exists
+	return app_tables.leaderboard.get(team_no = team_no)[team_score]
