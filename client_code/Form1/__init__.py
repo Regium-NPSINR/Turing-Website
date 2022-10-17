20af1f7e1688bbdf6f22b65dd8eb366206fc7a80
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-17 06:55:05 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,7 +15,7 @@ class Form1(Form1Template):
 		self.leaderboard_list.items = anvil.server.call('get_leaderboard')
 
 		# Initialize timer
-		self.time_left = 648000
+		self.time_left = app_tables.timer.get(id=1)['time_left']
 
 	def timer_1_tick(self, **event_args):
 		"""This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
