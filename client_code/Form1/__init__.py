0e7875b689dac4b81abe7f2a9b10981fece89cd0
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-18 04:50:01 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -20,7 +20,7 @@ class Form1(Form1Template):
 	def timer_1_tick(self, **event_args):
 		"""This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
 		self.time_left -= 1
-		self.time_remaining.text = datetime.strptime(str(timedelta(seconds=self.time_left)), '%I:%M:%S').strftime("%I:%M" if )"
+		self.time_remaining.text = datetime.strptime(str(timedelta(seconds=self.time_left)), '%I:%M:%S').strftime("%I:%M")
 
 	def on_refresh_leaderboard(self, **event_args):
 		"""This method is called when the refresh_leaderboard button is called"""
