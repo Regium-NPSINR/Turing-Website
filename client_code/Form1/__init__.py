fce6bae39c6825177aeb6292f8e69dba5b91338e
cyberassociation.npsinr@gmail.com <cyberassociation.npsinr@gmail.com>
2022-10-18 04:42:38 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -4,7 +4,7 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from datetime import timedelta
+import datetime
 
 class Form1(Form1Template):
 	def __init__(self, **properties):
@@ -20,7 +20,7 @@ class Form1(Form1Template):
 	def timer_1_tick(self, **event_args):
 		"""This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
 		self.time_left -= 1
-		self.time_remaining.text = str(timedelta(seconds=self.time_left))
+		self.time_remaining.text = datetime.strptime(timedelta(seconds=self.time_left), '%I:%M')
 
 	def on_refresh_leaderboard(self, **event_args):
 		"""This method is called when the refresh_leaderboard button is called"""
