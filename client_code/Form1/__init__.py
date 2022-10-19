6f056078306679864cc0fad9a4c03eb4886ee272
NPS INR Cyber Association <112054161+NPSINRCyberAssociation@users.noreply.github.com>
2022-10-19 09:01:59 +0530

    fix indentation

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -7,36 +7,34 @@ from anvil.tables import app_tables
 from datetime import datetime, timedelta
 
 class Form1(Form1Template):
-	def __init__(self, **properties):
-		# Set Form properties and Data Bindings.
-		self.init_components(**properties)
+    def __init__(self, **properties):
+        # Set Form properties and Data Bindings.
+        self.init_components(**properties)
 
-		# Any code you write here will run when the form opens.
-		self.leaderboard_list.items = anvil.server.call('get_leaderboard')
-		self.total_flags = 50
-		self.flags_left.text = self.total_flags
+        # Any code you write here will run when the form opens.
+        self.leaderboard_list.items = anvil.server.call('get_leaderboard')
+        self.total_flags = 50
+        self.flags_left.text = self.total_flags
 
-		# Initialize timer
-		self.end_time = datetime(2022, 10, 18, 15, 40)
-		self.timer_1.interval = 1
+        # Initialize timer
+        self.end_time = datetime(2022, 10, 18, 15, 40)
+        self.timer_1.interval = 1
 
-	def timer_1_tick(self, **event_args):
-		"""This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-		self.time_remaining.text = datetime.fromtimestamp((self.end_time - datetime.now()).total_seconds() - 19800).strftime("%M:%S" if (self.end_time - datetime.now()).total_seconds() < 3600 else "%H:%M")
-		self.timer_1.interval = 0 if self.time_remaining.text == '00:00' else 1
+    def timer_1_tick(self, **event_args):
+        """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+        self.time_remaining.text = datetime.fromtimestamp((self.end_time - datetime.now()).total_seconds() - 19800).strftime("%M:%S" if (self.end_time - datetime.now()).total_seconds() < 3600 else "%H:%M")
+        self.timer_1.interval = 0 if self.time_remaining.text == '00:00' else 1
 
-	def on_refresh_leaderboard(self, **event_args):
-		"""This method is called when the refresh_leaderboard button is called"""
-		self.update_leaderboard()
-	
-	def update_leaderboard(self):
-		"""This method updates the leaderboard"""
-		self.leaderboard_list.items = anvil.server.call('get_leaderboard')
-		self.refresh_data_bindings()
+    def on_refresh_leaderboard(self, **event_args):
+        """This method is called when the refresh_leaderboard button is called"""
+        self.update_leaderboard()
+    
+    def update_leaderboard(self):
+        """This method updates the leaderboard"""
+        self.leaderboard_list.items = anvil.server.call('get_leaderboard')
+        self.refresh_data_bindings()
 
-	def leaderboard_refresh_timer_tick(self, **event_args):
-		"""This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
-		self.update_leaderboard()
-		self.flags_left.text = str(self.total_flags - sum([r['team_score'] for r in self.leaderboard_list.items]))
-        
-
+    def leaderboard_refresh_timer_tick(self, **event_args):
+        """This method is called Every [interval] seconds. Does not trigger if [interval] is 0."""
+        self.update_leaderboard()
+        self.flags_left.text = str(self.total_flags - sum([r['team_score'] for r in self.leaderboard_list.items]))
--- server_code/ServerModule.py
+++ server_code/ServerModule.py
@@ -18,62 +18,62 @@ import anvil.server
 
 @anvil.server.callable
 def add_team(team_name):
-	if app_tables.leaderboard.get(team_name = team_name) != None:
-		return False
+    if app_tables.leaderboard.get(team_name = team_name) != None:
+        return False
 
-	app_tables.leaderboard.add_row(
-		team_name = team_name,
-		team_score = 0
-	)
-	return True
+    app_tables.leaderboard.add_row(
+        team_name = team_name,
+        team_score = 0
+    )
+    return True
 
 @anvil.server.callable
 def set_score(team_name, score):
-	# check if the team exists
-	if app_tables.leaderboard.get(team_name = team_name) == None:
-		return False
-	
-	# Team already exists
-	app_tables.leaderboard.get(team_name = team_name)['team_score'] = score
-	return True
+    # check if the team exists
+    if app_tables.leaderboard.get(team_name = team_name) == None:
+        return False
+    
+    # Team already exists
+    app_tables.leaderboard.get(team_name = team_name)['team_score'] = score
+    return True
 
 @anvil.server.callable
 def solve_easy(team_name):
-	# check if the team exists
-	if app_tables.leaderboard.get(team_name = team_name) == None:
-		return False
+    # check if the team exists
+    if app_tables.leaderboard.get(team_name = team_name) == None:
+        return False
 
-	# Team already exists
-	print(app_tables.leaderboard.get(team_name = team_name))
-	print(dict(app_tables.leaderboard.get(team_name = team_name)))
-	app_tables.leaderboard.get(team_name = team_name)['team_score'] += 1
-	return True
+    # Team already exists
+    print(app_tables.leaderboard.get(team_name = team_name))
+    print(dict(app_tables.leaderboard.get(team_name = team_name)))
+    app_tables.leaderboard.get(team_name = team_name)['team_score'] += 1
+    return True
 
 @anvil.server.callable
 def solve_hard(team_name_1, team_name_2):
-	# check if the teams exist
-	if ((app_tables.leaderboard.get(team_name = team_name_1) == None) or
-		(app_tables.leaderboard.get(team_name = team_name_2) == None)):
-		return False
+    # check if the teams exist
+    if ((app_tables.leaderboard.get(team_name = team_name_1) == None) or
+        (app_tables.leaderboard.get(team_name = team_name_2) == None)):
+        return False
 
-	# Teams already exists
-	app_tables.leaderboard.get(team_name = team_name_1)['team_score'] += 1
-	app_tables.leaderboard.get(team_name = team_name_2)['team_score'] -= 1
-	return True
+    # Teams already exists
+    app_tables.leaderboard.get(team_name = team_name_1)['team_score'] += 1
+    app_tables.leaderboard.get(team_name = team_name_2)['team_score'] -= 1
+    return True
 
 @anvil.server.callable
 def get_score(team_name):
-	# check if the team exists
-	if app_tables.leaderboard.get(team_name = team_name) == None:
-		return False
+    # check if the team exists
+    if app_tables.leaderboard.get(team_name = team_name) == None:
+        return False
 
-	# Team exists
-	return app_tables.leaderboard.get(team_name = team_name)['team_score']
+    # Team exists
+    return app_tables.leaderboard.get(team_name = team_name)['team_score']
 
 @anvil.server.callable
 def get_leaderboard():
-	return sorted(
-			app_tables.leaderboard.search(),
-			key = lambda x: (x['team_score'], x['team_name']),
-			reverse = True
-		)
+    return sorted(
+            app_tables.leaderboard.search(),
+            key = lambda x: (x['team_score'], x['team_name']),
+            reverse = True
+        )
